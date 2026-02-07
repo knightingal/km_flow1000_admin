6c0eee97c54d1adaee5823ee2ce2a619573fc6cb
knightingal <yujianming1985@hotmail.com>
2026-02-07 17:38:27 +0800

    add boxes in InnerPage

--- composeApp/src/commonMain/kotlin/org/nanking/km_flow1000_admin/InnerPage.kt
+++ composeApp/src/commonMain/kotlin/org/nanking/km_flow1000_admin/InnerPage.kt
@@ -1,16 +1,21 @@
 package org.nanking.km_flow1000_admin
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
@@ -26,6 +31,42 @@ fun InnerPage(navController: NavHostController) {
         Button(onClick = { navController.popBackStack() }) {
             Text("Back")
         }
+        Box(
+            modifier = Modifier
+                .height(16.dp)
+                .width(320.dp)
+                .background(color = Color.Red)
+        )
+        Box(
+            modifier = Modifier
+                .height(16.dp)
+                .width(320.dp)
+                .background(color = Color.Green)
+        )
+        Box(
+            modifier = Modifier
+                .height(16.dp)
+                .width(320.dp)
+                .background(color = Color.Blue)
+        )
+        Box(
+            modifier = Modifier
+                .height(16.dp)
+                .width(320.dp)
+                .background(color = Color.Yellow)
+        )
+        Box(
+            modifier = Modifier
+                .height(16.dp)
+                .width(320.dp)
+                .background(color = Color.Cyan)
+        )
+        Box(
+            modifier = Modifier
+                .height(16.dp)
+                .width(320.dp)
+                .background(color = Color.Magenta)
+        )
         Text("Inner Page")
     }
 
