718f0d51c8d322d35dbb456a129f227d38157737
knightingal <yujianming1985@hotmail.com>
2026-02-11 23:16:14 +0800

    enhance greeting page

--- composeApp/src/commonMain/kotlin/org/nanking/km_flow1000_admin/Greeting.kt
+++ composeApp/src/commonMain/kotlin/org/nanking/km_flow1000_admin/Greeting.kt
@@ -1,9 +1,44 @@
 package org.nanking.km_flow1000_admin
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kotlin.random.Random
+
 class Greeting {
     private val platform = getPlatform()
 
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
+    fun greet(): List<String> = buildList {
+        add(if (Random.nextBoolean()) "Hi!" else "Hello!")
+        add("Guess what this is! > ${platform.name.reversed()}!")
+    }
+}
+
+@Composable
+@Preview
+fun GreeingApp() {
+
+    val greeting = remember { Greeting().greet() }
+
+    Column(
+        modifier = Modifier
+            .padding(all = 10.dp)
+            .safeContentPadding()
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        greeting.forEach { greeting ->
+            Text(greeting)
+            HorizontalDivider()
+        }
     }
 }
