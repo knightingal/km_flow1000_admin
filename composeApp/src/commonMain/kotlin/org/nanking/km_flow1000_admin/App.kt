6ab210a5ea35975db0515301b3652f0c0c118e55
knightingal <yujianming1985@hotmail.com>
2026-02-14 23:52:47 +0800

    start request

--- composeApp/src/commonMain/kotlin/org/nanking/km_flow1000_admin/App.kt
+++ composeApp/src/commonMain/kotlin/org/nanking/km_flow1000_admin/App.kt
@@ -2,27 +2,21 @@ package org.nanking.km_flow1000_admin
 
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import km_flow1000_admin.composeapp.generated.resources.Res
-import km_flow1000_admin.composeapp.generated.resources.ab1_inversions
-import km_flow1000_admin.composeapp.generated.resources.fc1_short_mantras
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
         val navController = rememberNavController()
-        Scaffold {
+        Scaffold { it ->
+            it.hashCode()
             NavHost(navController, startDestination = "greeting",
                 enterTransition = { slideInHorizontally() },
                 exitTransition = { slideOutHorizontally(targetOffsetX = { -it }) },
@@ -32,7 +26,7 @@ fun App() {
                 composable ("greeting") {GreetingApp()}
             }
         }
-//        LazyPage()
+//        LazyPage(navController)
     }
 
 }
--- composeApp/src/commonMain/kotlin/org/nanking/km_flow1000_admin/Greeting.kt
+++ composeApp/src/commonMain/kotlin/org/nanking/km_flow1000_admin/Greeting.kt
@@ -8,11 +8,14 @@ import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
@@ -39,9 +42,12 @@ class Greeting {
 
 @Composable
 @Preview
-fun GreetingApp(mainViewModel: MainViewModel = viewModel()) {
+fun GreetingApp() {
 
-    val greeting by mainViewModel.greetingList.collectAsStateWithLifecycle()
+//    val greeting by mainViewModel.greetingList.collectAsStateWithLifecycle()
+
+    val rocketComponent = RocketComponent()
+    LaunchedEffect(true) { rocketComponent.launchPhrase() }
 
     Column(
         modifier = Modifier
