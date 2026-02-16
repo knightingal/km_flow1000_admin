ab54a30c7548c31e460707ad0c631e465e15a192
knightingal <yujianming1985@hotmail.com>
2026-02-16 13:49:05 +0800

    display AsyncImage

--- composeApp/src/commonMain/kotlin/org/nanking/km_flow1000_admin/Greeting.kt
+++ composeApp/src/commonMain/kotlin/org/nanking/km_flow1000_admin/Greeting.kt
@@ -1,10 +1,14 @@
 package org.nanking.km_flow1000_admin
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,6 +23,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil3.compose.AsyncImage
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -45,13 +50,13 @@ class Greeting {
 @Composable
 @Preview
 fun GreetingApp() {
-
-//    val greeting by mainViewModel.greetingList.collectAsStateWithLifecycle()
     var text by remember { mutableStateOf("Loading") }
-
+    var picIndexList by remember { mutableStateOf(listOf<PicIndexItem>()) }
     val rocketComponent = RocketComponent()
-    LaunchedEffect(true) { text = rocketComponent.launchPhrase() }
-
+    LaunchedEffect(true) {
+        text = rocketComponent.launchPhrase()
+        picIndexList = rocketComponent.fetchPicIndex()
+    }
     Column(
         modifier = Modifier
             .padding(all = 10.dp)
@@ -61,5 +66,12 @@ fun GreetingApp() {
     ) {
         Text(text = text)
         HorizontalDivider()
+        for (picIndexItem in picIndexList) {
+//            Text(picIndexItem.name, Modifier.padding(bottom = 5.dp))
+            AsyncImage(model = picIndexItem.coverUrl(), contentDescription = null,
+                modifier = Modifier
+                    .height((picIndexItem.coverHeight/4).dp)
+                    .width((picIndexItem.coverWidth/4).dp))
+        }
     }
 }
