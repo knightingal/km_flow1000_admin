e67cc87fcada9f81d244a4ee74ad7f9b029e2f23
knightingal <yujianming1985@hotmail.com>
2026-02-17 20:18:55 +0800

    implement logger interface for jvm and android

--- composeApp/src/androidMain/kotlin/org/nanking/km_flow1000_admin/Platform.android.kt
+++ composeApp/src/androidMain/kotlin/org/nanking/km_flow1000_admin/Platform.android.kt
@@ -1,9 +1,35 @@
 package org.nanking.km_flow1000_admin
 
 import android.os.Build
+import android.util.Log
 
 class AndroidPlatform : Platform {
     override val name: String = "Android ${Build.VERSION.SDK_INT}"
 }
 
 actual fun getPlatform(): Platform = AndroidPlatform()
+
+actual fun getLogger(): Logger = AndroidLogger()
+
+class AndroidLogger : Logger {
+    override fun d(tag: String, message: () -> String) {
+        Log.d(tag, message())
+    }
+
+    override fun w(tag: String, message: () -> String) {
+        Log.w(tag, message())
+    }
+
+    override fun i(tag: String, message: () -> String) {
+        Log.i(tag, message())
+    }
+
+    override fun e(tag: String, message: () -> String) {
+        Log.e(tag, message())
+    }
+
+    override fun e(tag: String, message: () -> String, error: Throwable) {
+        Log.e(tag, message(), error)
+    }
+
+}
--- composeApp/src/jvmMain/kotlin/org/nanking/km_flow1000_admin/Platform.jvm.kt
+++ composeApp/src/jvmMain/kotlin/org/nanking/km_flow1000_admin/Platform.jvm.kt
@@ -1,7 +1,40 @@
 package org.nanking.km_flow1000_admin
 
+import org.slf4j.LoggerFactory
+
 class JVMPlatform : Platform {
     override val name: String = "Java ${System.getProperty("java.version")}"
 }
 
 actual fun getPlatform(): Platform = JVMPlatform()
+
+actual fun getLogger(): Logger = JVMLogger()
+
+class JVMLogger: Logger {
+
+    override fun d(tag: String, message: () -> String) {
+        val logger = LoggerFactory.getLogger(tag)
+        logger.debug(message())
+    }
+
+    override fun w(tag: String, message: () -> String) {
+        val logger = LoggerFactory.getLogger(tag)
+        logger.warn(message())
+    }
+
+    override fun i(tag: String, message: () -> String) {
+        val logger = LoggerFactory.getLogger(tag)
+        logger.info(message())
+    }
+
+    override fun e(tag: String, message: () -> String) {
+        val logger = LoggerFactory.getLogger(tag)
+        logger.error(message())
+    }
+
+    override fun e(tag: String, message: () -> String, error: Throwable) {
+        val logger = LoggerFactory.getLogger(tag)
+        logger.error(message(), error)
+    }
+
+}
