4acf6d765fcf81d15af982b47f0611c05195bc08
knightingal <yujianming1985@hotmail.com>
2026-02-17 20:18:20 +0800

    create Logger interface

--- composeApp/src/commonMain/kotlin/org/nanking/km_flow1000_admin/Platform.kt
+++ composeApp/src/commonMain/kotlin/org/nanking/km_flow1000_admin/Platform.kt
@@ -5,3 +5,13 @@ interface Platform {
 }
 
 expect fun getPlatform(): Platform
+
+expect fun getLogger(): Logger
+
+interface Logger {
+    fun d(tag: String, message: () -> String)
+    fun w(tag: String, message: () -> String)
+    fun i(tag: String, message: () -> String)
+    fun e(tag: String, message: () -> String)
+    fun e(tag: String, message: () -> String, error: Throwable)
+}
