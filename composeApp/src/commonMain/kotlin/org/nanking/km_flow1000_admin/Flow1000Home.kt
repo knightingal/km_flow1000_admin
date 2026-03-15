c3de9f59c2e9221cfb505287e4621090096b0764
knightingal <yujianming1985@hotmail.com>
2026-03-15 16:07:44 +0800

    reformat code

--- composeApp/src/commonMain/kotlin/org/nanking/km_flow1000_admin/Flow1000Home.kt
+++ composeApp/src/commonMain/kotlin/org/nanking/km_flow1000_admin/Flow1000Home.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,13 +16,10 @@ import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.rememberLazyStaggeredGridState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -40,10 +36,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.key.Key
-import androidx.compose.ui.input.key.key
-import androidx.compose.ui.input.key.onKeyEvent
-import androidx.compose.ui.input.key.onPreviewKeyEvent
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -52,12 +44,8 @@ import km_flow1000_admin.composeapp.generated.resources.Res
 import km_flow1000_admin.composeapp.generated.resources.fc6_nightly_wind_down
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
-import androidx.compose.ui.input.key.key
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.compose.viewModel
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -125,61 +113,59 @@ fun Flow1000AlbumPage(navController: NavHostController, albumConfig: AlbumParam)
     LaunchedEffect(true) {
         pinIndexList = rocketComponent.fetchPicIndex(albumConfig.name)
     }
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    navigationIcon = {
-                        IconButton(onClick = { navController.popBackStack() }) {
-                            Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null)
-                        }
-                    },
-                    colors = topAppBarColors(
-                        containerColor = MaterialTheme.colorScheme.primaryContainer,
-                        titleContentColor = MaterialTheme.colorScheme.primary,
-                    ),
-                    title = {
-                        Text(albumConfig.name)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null)
                     }
-                )
-            },
-        ) { it ->
-                Box(
-                    modifier = Modifier
-                        .background(MaterialTheme.colorScheme.primaryContainer)
-                        .systemBarsPadding()
-                        .fillMaxSize()
-                        .padding(top = it.calculateTopPadding(), )
-                ) {
-                    val lazyStaggeredGridState = rememberLazyStaggeredGridState()
-                    LazyVerticalStaggeredGrid(
-                        state = lazyStaggeredGridState,
-                        columns = StaggeredGridCells.Fixed(2),
-                        verticalItemSpacing = 4.dp,
-                        horizontalArrangement = Arrangement.spacedBy(4.dp),
-                        modifier = Modifier.fillMaxSize().padding(horizontal = 4.dp, vertical = 0.dp),
-                    ) {
-                        items(pinIndexList.size) { index ->
-                            val coverUrl = pinIndexList[index].cover
-                            logger.i { "Display Cover URL: $coverUrl" }
-                            val picIndex = pinIndexList[index]
-                            picIndex.albumSourcePath = albumConfig.albumSourcePath
-                            AlbumCoverCard(albumConfig = picIndex) {
-                            }
-                        }
-                    }
-                    PlatformVerticalScrollbar(
-                        modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight(),
-                        lazyStaggeredGridState = lazyStaggeredGridState
-                    )
-
+                },
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(albumConfig.name)
                 }
+            )
+        },
         floatingActionButton = {
             IconButton(onClick = { navController.popBackStack() }) {
                 Icon(Icons.Filled.Add, contentDescription = null)
             }
         }
+    ) {
+        Box(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.primaryContainer)
+                .systemBarsPadding()
+                .fillMaxSize()
+                .padding(top = it.calculateTopPadding())
+        ) {
+            val lazyStaggeredGridState = rememberLazyStaggeredGridState()
+            LazyVerticalStaggeredGrid(
+                state = lazyStaggeredGridState,
+                columns = StaggeredGridCells.Fixed(2),
+                verticalItemSpacing = 4.dp,
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                modifier = Modifier.fillMaxSize().padding(horizontal = 4.dp, vertical = 0.dp),
+            ) {
+                items(pinIndexList.size) { index ->
+                    val coverUrl = pinIndexList[index].cover
+                    logger.i { "Display Cover URL: $coverUrl" }
+                    val picIndex = pinIndexList[index]
+                    picIndex.albumSourcePath = albumConfig.albumSourcePath
+                    AlbumCoverCard(albumConfig = picIndex) {
+                    }
+                }
+            }
+            PlatformVerticalScrollbar(
+                modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight(),
+                lazyStaggeredGridState = lazyStaggeredGridState
+            )
+        }
     }
-
 }
 
 @Composable
