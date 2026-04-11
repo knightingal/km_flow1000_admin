d47d970b00a4631d0df50af7d9f1dcc540fb22b4
knightingal <yujianming1985@hotmail.com>
2026-04-11 16:58:28 +0800

    clean `RocketComponent`

--- composeApp/src/commonMain/kotlin/org/nanking/km_flow1000_admin/RocketComponent.kt
+++ composeApp/src/commonMain/kotlin/org/nanking/km_flow1000_admin/RocketComponent.kt
@@ -16,14 +16,6 @@ import io.ktor.client.request.post
 
 
 class RocketComponent {
-    companion object {
-        const val FLOW_1000_INDEX_URL = "http://$API_SERVER/local1000/picIndexAjax?album=%s"
-        const val FLOW_1000_COVER_LIST_URL = "http://$API_SERVER/local1000/albumConfig/list"
-        const val FLOW_1000_SECTION_CONTENT_URL = "http://$API_SERVER/local1000/picDetailAjax?id=%d"
-        const val FLOW_1000_SECTION_DOWNLOAD_URL = "http://$API_SERVER/local1000/downloadSection?id=%d"
-        val json = Json { ignoreUnknownKeys = true }
-        val logger = getLogger("RocketComponent")
-    }
 
     private val httpClient = HttpClient(OkHttp) {
         install(ContentNegotiation) {
@@ -34,31 +26,6 @@ class RocketComponent {
             })
         }
     }
-
-    suspend fun fetchAlbumConfigList(): List<AlbumConfig> {
-        logger.d { "Fetching AlbumConfigList" }
-        val responseBody: String = httpClient.get(FLOW_1000_COVER_LIST_URL).body()
-        return json.decodeFromString<List<AlbumConfig>>(responseBody)
-    }
-
-    suspend fun fetchPicIndex(albumName: String): List<PicIndexItem> {
-        logger.d { "Fetching picIndex" }
-        val responseBody: String = httpClient.get(String.format(FLOW_1000_INDEX_URL, albumName)).body()
-        return json.decodeFromString<List<PicIndexItem>>(responseBody)
-    }
-
-    suspend fun fetchSectionContent(id: Long): SectionDetail {
-        logger.d { "Fetching section content" }
-        val response: SectionDetail = httpClient.get(String.format(FLOW_1000_SECTION_CONTENT_URL, id)).body()
-        return response
-    }
-
-    suspend fun downloadSectionById(id: Long) {
-        logger.d { "Download section by id $id" }
-        val response = httpClient.post (String.format(FLOW_1000_SECTION_DOWNLOAD_URL, id))
-        logger.d { "Download section response ${response.status.value}" }
-    }
-
 
     @OptIn(ExperimentalTime::class)
     private suspend fun getDateOfLastSuccessfulLaunch(): String {
